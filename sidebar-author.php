f62cd36c5e036f445be76667f46bf9c15cad9020
Kannami <xfire.radeon5rx@gmail.com>
2025-03-30 23:18:32 +0900

    add sns icon

--- sidebar-author.php
+++ sidebar-author.php
@@ -6,4 +6,9 @@
             <!--<img src="<?php echo get_template_directory_uri(); ?>/images/Author.png" alt="Author" width="109" height="109"> -->
             <p class="sidebar-p">
             もう30代。日々の仕事や人生の考えごと、ハードウェア開発の備忘録、革靴・メガネといった暮らしの道具、そして学生時代から趣味にしているミステリ小説の感想をゆるくまとめています。お問い合わせはページ下部のCONTACTからお願いします。</p>
+            
+            <p class="sidebar-p">
+            <i class="fa-brands fa-square-x-twitter"></i>  <a href="https://x.com/kannami_green/" target="_blank">X</a><br>
+            <i class="fa-solid fa-n"></i>  <a href="https://note.com/kannaminote" target="_blank">note</a><br>
+            <i class="fa-brands fa-square-instagram"></i>  <a href="https://www.instagram.com/kannami33333/" target="_blank">Instagram</a></p>
         </div>
